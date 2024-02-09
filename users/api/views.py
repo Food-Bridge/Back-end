c4ff797c5d9554fec2e7af22b4956558a5902a1c
initNirvana <kimqqyun@gmail.com>
2024-02-10 00:56:18 +0900

    feat: 카카오 로그인 기능 추가

--- users/api/views.py
+++ users/api/views.py
@@ -2,18 +2,27 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import exceptions
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.renderers import JSONRenderer
+from django.shortcuts import redirect
+from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.views import APIView
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication, CSRFCheck
+from rest_framework.authentication import CSRFCheck
 from users.api.serializers import RegisterSerializer, LoginSerializer, LogoutSerializer, AddressSerializer, OrderSerializer
 from users.models import User, Address, Order
-from django.contrib.auth import authenticate
 from django.http import JsonResponse
-import requests, json
+import requests
+from django.contrib import messages
+from json.decoder import JSONDecodeError
 from urllib.parse import urlparse
+from django.conf import settings
+
+from allauth.socialaccount.models import SocialAccount
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from allauth.socialaccount.providers.kakao import views as kakao_view
 
 # Create your views here.
 ##### 시리얼라이저를 사용해서 유저를 저장하고(=회원가입), JWT토큰을 발급받아 쿠키에 저장한다.
@@ -108,3 +117,46 @@ class UserOrderAPIView(generics.ListAPIView):
     def get_queryset(self):
         user_id = self.kwargs['user_id']
         return Order.objects.filter(user_id=user_id)
+
+##### 카카오 로그인창을 띄우고, 사용자가 카카오 계정으로 로그인을 하면 인증 코드를 받아오는 함수
+##### 토큰 받기
+##### 토큰을 기반으로 사용자 로그인 처리
+class GetKakaoAccessView(APIView):
+    def post(self, request, *args, **kwargs):
+        kakao_access_token = request.data.get("access_token")
+        ##### Profile request
+        profile_request = requests.get(
+            "https://kapi.kakao.com/v2/user/me", headers={"Authorization": f"Bearer {kakao_access_token}"})
+        profile_json = profile_request.json()
+        kakao_account = profile_json.get('kakao_account')
+        ##### Email
+        email = kakao_account.get('email')
+        try:
+            user = User.objects.get(email=email)
+            message = "카카오 소셜 로그인 완료"
+        except User.DoesNotExist:
+            user = User.objects.create(email=email)
+            message = "회원가입 완료"
+
+        ##### jwt 토큰
+        token = TokenObtainPairSerializer.get_token(user)
+        refresh_token = str(token)
+        access_token = str(token.access_token)
+        res = Response(
+            {
+                "user" : email,
+                "message" : message,
+                "token" : {
+                    "access" : access_token,
+                    "refresh" : refresh_token,
+                },
+            },
+            status = status.HTTP_200_OK,
+        )
+        res.renderer_context = {}
+        res.accepted_renderer = JSONRenderer()
+        res.accepted_media_type = "application/json"
+        ##### JWT 토큰 쿠키에 저장
+        res.set_cookie("access", access_token, httponly=True)
+        res.set_cookie("refresh", refresh_token, httponly=True)
+        return res
