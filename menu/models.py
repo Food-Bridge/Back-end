647d0b44d2cf04cf5bb7a4fc5197a2f2ccedd90d
initNirvana <kimqqyun@gmail.com>
2024-03-03 16:01:33 +0900

    feat: 메뉴 설명 필드 및 옵션 radio/option 값 추가

--- menu/models.py
+++ menu/models.py
@@ -6,7 +6,7 @@ class Menu(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     price = models.PositiveIntegerField()
-    rating = models.FloatField()
+    content = models.CharField(max_length=150, blank=True, null=True) # 메뉴 상세 내용
     image = models.ImageField(upload_to='restaurant_image/menu/%Y/%m/%d', blank=True, null=True)
     is_main = models.BooleanField(default=False)
     is_popular = models.BooleanField(default=False)
@@ -18,8 +18,14 @@ class MenuOption(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='options')
     name = models.CharField(max_length=100)
     price = models.PositiveIntegerField()
-    is_checked = models.BooleanField(default=False)
-    check_option = models.PositiveIntegerField(default=1, validators=[MaxValueValidator(3)])
+    is_required = models.BooleanField(default=False)
+    
+    BUTTON_TYPE = [
+        ('option', 'Option'), 
+        ('radio', 'Radio')
+    ]
+    
+    type = models.CharField(max_length=10, choices=BUTTON_TYPE, default='option')
 
     def __str__(self):
         return self.name
