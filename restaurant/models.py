aff750b02b2f9545caf5c78b0f484ac8abf6985c
initNirvana <kimqqyun@gmail.com>
2024-03-03 16:00:36 +0900

    feat: 매장 모델 수정

--- restaurant/models.py
+++ restaurant/models.py
@@ -25,31 +25,36 @@ class SubCategory(models.Model):
 
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name="매장 이름")
+    address = models.CharField(max_length=255, verbose_name="매장 주소")
     phone_number_regex = RegexValidator(regex=r'^02?-?\d{3,4}-?\d{4}$')
-    phone_number= models.CharField(validators = [phone_number_regex], max_length =13, unique=True)
+    phone_number= models.CharField(validators = [phone_number_regex], max_length =13, unique=True, verbose_name="매장 전화번호")
     image = models.ImageField(default='default_restaurant.png', upload_to='restaurant_image/%Y/%m/%d', blank=True, null=True)
-    description = models.CharField(max_length=255, blank=True)
-    minimumOrderPrice = models.IntegerField()
-    minDeliveryTime = models.TimeField(null=True)
-    maxDeliveryTime = models.TimeField(null=True)
-    orderCount = models.IntegerField(default=0)
-    reviewCount = models.IntegerField(default=0)
-    bookmarkCount = models.IntegerField(default=0)
+    description = models.CharField(max_length=255, blank=True, verbose_name="매장 설명")
+    minimumOrderPrice = models.PositiveIntegerField(default=0, verbose_name="최소 주문 금액")
+    
+    minDeliveryTime = models.PositiveSmallIntegerField(default=0, verbose_name="최소 배달 시간")
+    maxDeliveryTime = models.PositiveSmallIntegerField(default=0, verbose_name="최대 배달 시간") 
+    
+    minPickupTime = models.PositiveIntegerField(default=0, null=True, verbose_name="최소 픽업 시간")
+    orderCount = models.PositiveIntegerField(default=0, verbose_name="주문수")
+    reviewCount = models.PositiveIntegerField(default=0, verbose_name="리뷰수")
+    bookmarkCount = models.PositiveIntegerField(default=0, verbose_name="즐겨찾기수")
     rating = models.FloatField(default=0, validators=[MinValueValidator(0, 5), MaxValueValidator(5.0)])
-    packaging = models.BooleanField(default=False)
-    status = models.BooleanField(default=False)
-    start = models.IntegerField(default=0)
-    end = models.IntegerField(default=0)
-    operatingTime = models.CharField(max_length=255)
-    delivertyFee = models.PositiveIntegerField(default=0)
+    packaging = models.BooleanField(default=False, verbose_name="포장 여부")
+    status = models.BooleanField(default=False, verbose_name="매장 운영 여부")
+    start = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0, 24), MaxValueValidator(24)], verbose_name="영업 시작 시간")
+    end = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0, 36), MaxValueValidator(36)], verbose_name="영업 마감 시간")
+    operatingTime = models.CharField(max_length=255, verbose_name="운영 시간 설명")
+    delivertyFee = models.PositiveIntegerField(default=0, verbose_name="배달 요금")
+    
     mainCategory = models.ForeignKey(
         MainCategory,
         on_delete=models.SET_DEFAULT,
         related_name='mainCategory',
         blank=True,
         default=None,
+        verbose_name="매장 대분류"
     )
     subCategory = models.ForeignKey(
         SubCategory,
@@ -57,11 +62,12 @@ class Restaurant(models.Model):
         related_name='subCategory',
         blank=True,
         null=True,
+        verbose_name="매장 중분류"
     )
     
     class Meta:
-        verbose_name = "음식점"
-        verbose_name_plural = "음식점"
+        verbose_name = "매장"
+        verbose_name_plural = "매장"
 
     def __str__(self):
         return self.name
