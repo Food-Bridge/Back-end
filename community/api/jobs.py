c07a2f1bebf0e5465082da1626da28a1291eeb3c
dnwls16071 <dnwls16071@naver.com>
2024-03-06 02:39:40 +0900

    style: 불필요한 import문 정리

--- community/api/jobs.py
+++ community/api/jobs.py
@@ -1,11 +1,3 @@
-from django.utils import timezone
-from datetime import timedelta
-from django.db.models import Count, F
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .serializers import PopularPostSerializer
-from community.models import Blog
 from community.api.views import DailyPopularPostAPIView
 from apscheduler.schedulers.background import BackgroundScheduler
 from django.conf import settings
