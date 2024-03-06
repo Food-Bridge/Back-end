2a5d30066bfbe453cf509574b4e905c11a673221
initNirvana <kimqqyun@gmail.com>
2024-03-06 22:44:33 +0900

    feat: 장바구니 API, 주문 API와 유사하게 처리

--- cart/api/serializers.py
+++ cart/api/serializers.py
@@ -2,8 +2,7 @@ from rest_framework import serializers
 from cart.models import cart
 
 class CartSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source="user.id")
     
     class Meta:
         model = cart
-        fields = "__all__" #['id', 'user', 'menu_id', 'menu_name', 'menu_price', 'amount', 'total_price', 'option_name', 'option_price', 'restaurant_id']
+        fields = "__all__"
--- cart/api/views.py
+++ cart/api/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from cart.models import cart
+from restaurant.models import Restaurant
+from menu.models import Menu, MenuOption
 from .serializers import CartSerializer
 
 class CartAPIView(generics.RetrieveUpdateDestroyAPIView):
@@ -14,34 +16,39 @@ class CartAPIView(generics.RetrieveUpdateDestroyAPIView):
     6. restaurant : 레스토랑 id
     
     메뉴 2개, 옵션 2개 이상일 경우의 예
-    ```
     {
-    "menu_list":
+    "menu_list": [
         {
-            "menu_id: 1,
-            "menu_name" : "양념치킨",
-            "price" : 15000 ,
-            "quantity" : 1 
+        "menu_id": 1,
+        "menu_name": "양념치킨",
+        "price": 15000,
+        "quantity": 1
         },
         {
-            "menu_id": 3,
-            "menu" : "간장치킨",
-            "price" : 17000,
-            "quantity" : 1
+        "menu_id": 3,
+        "menu_name": "간장치킨",
+        "price": 17000,
+        "quantity": 1
         }
-    "option_list":
+    ],
+    "option_list": [
         {
-            "option_id": 1,
-            "option_name" : "소스 추가"
-            "price" : 1000 ,
-            "quantity" : 1
+        "option_id": 1,
+        "option_name": "소스 추가",
+        "price": 1000,
+        "quantity": 1
         },
         {
-            "option_id": 10,
-            "option_name" : "치킨 무 추가"
-            "price" : 0,
-            "quantity" : 1
-        },
+        "option_id": 10,
+        "option_name": "치킨 무 추가",
+        "price": 0,
+        "quantity": 1
+        }
+    ],
+    "total_price": 0,
+    "restaurant": 1
+    }
+
     ...
     ```
     }
@@ -61,18 +68,65 @@ class CartAPIView(generics.RetrieveUpdateDestroyAPIView):
             return None
 
     def post(self, request, *args, **kwargs):
-        user = self.request.user
+        restaurant_id = request.data.get('restaurant')
+        menu_data = request.data.get('menu_list', [])  # 메뉴 데이터 가져오기
+        option_data = request.data.get('option_list', [])  # 옵션 데이터 가져오기
+        
+        # 장바구니에 저장할 음식점이 유효한지 확인
+        try:
+            restaurant = Restaurant.objects.get(id=restaurant_id)
+        except Restaurant.DoesNotExist:
+            return Response({'error': '유효하지 않은 음식점입니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # 메뉴와 옵션을 저장할 리스트 초기화
+        menus = []
+        options = []
+        total_price = 0
+        
+        if len(menu_data) == 0:
+            return Response({'error': "필수 주문 데이터가 존재하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # 메뉴 데이터 처리
+        for menu_item in menu_data:
+            menu_id = menu_item.get('menu_id')
+            menu_price = menu_item.get('price')
+            menu_quantity = menu_item.get('quantity')
+            try:
+                menu = Menu.objects.get(id=menu_id, restaurant=restaurant)
+                menus.append(menu)
+                total_price += menu_price * menu_quantity  # 메뉴 가격과 수량을 곱하여 총 가격에 더함
+            except Menu.DoesNotExist:
+                return Response({'error': '음식점에 속하지 않은 메뉴가 주문되었습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 옵션 데이터 처리
+        for option_item in option_data:
+            option_id = option_item.get('option_id')
+            option_price = option_item.get('price')
+            option_quantity = option_item.get('quantity')
+            try:
+                option = MenuOption.objects.get(id=option_id)
+                options.append(option)
+                total_price += option_price * option_quantity  # 옵션 가격과 수량을 곱하여 총 가격에 더함
+            except MenuOption.DoesNotExist:
+                return Response({'error': '존재하지 않는 옵션이 주문되었습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # 장바구니 데이터 생성
+        cart_data = {
+            'user': request.user.id,
+            'restaurant': request.data.get('restaurant'),
+            'menu_list' : menu_data,
+            'option_list' : option_data,
+            'total_price': total_price,
+        }
+        cart_serializer = CartSerializer(data=cart_data)
         
         # 기존 장바구니에 데이터 있을 경우 데이터 삭제
         existing_cart = self.get_object()
         if existing_cart:
             existing_cart.delete() 
         
-        if user.is_authenticated:
-            request.data["user"] = user.id
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            serializer.save(user=user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if cart_serializer.is_valid():
+            cart_serializer.save()
+            return Response(cart_serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response({"message": "인증이 필요합니다."}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(cart_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
