8fa00de61a1829f43fdc71e1e6820f65790a0afd
Nirvana <kimqqyun@gmail.com>
2024-03-15 15:46:24 +0900

    feat: Order, restaurant_name 추가

--- order/api/serializers.py
+++ order/api/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from order.models import Order
 
 class OrderSerializer(serializers.ModelSerializer):
+    restaurant_name = serializers.ReadOnlyField(source="restaurant.name")
         
     class Meta:
         model = Order
-        fields = ["user", "required_options_count", "menu_list", "option_list", "total_price", "deliveryman_request", "paymentMethod", "order_state", "created_at", "restaurant", "order_id"]
+        fields = ["user", "restaurant_name", "required_options_count", "menu_list", "option_list", "total_price", "deliveryman_request", "paymentMethod", "order_state", "created_at", "restaurant", "order_id"]
