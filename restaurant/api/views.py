768c6dfc1b53d732c67b6abcf980a05d0c23a09e
initNirvana <kimqqyun@gmail.com>
2024-03-25 20:04:40 +0900

    feat: 레스토랑에 속한 리뷰 API 추가

--- restaurant/api/views.py
+++ restaurant/api/views.py
@@ -1,6 +1,10 @@
+from rest_framework import permissions, generics
+
 from restaurant.api.serializers import RestaurantSerializer
 from restaurant.models import Restaurant
-from rest_framework import permissions, generics
+from review.models import Review
+from review.api.serializers import ReviewSerializer
+
 
 class RestaurantAPIView(generics.ListCreateAPIView):
     permission_classes = [permissions.AllowAny]
@@ -11,3 +15,11 @@ class RestaurantDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = RestaurantSerializer
     queryset = Restaurant.objects.all()
+    
+class RestaurantReviewAPIView(generics.ListAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ReviewSerializer
+    
+    def get_queryset(self):
+        restaurant_id = self.kwargs['pk']
+        return Review.objects.filter(restaurant_id=restaurant_id)
