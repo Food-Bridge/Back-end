c62f8c33301b1147c937322c578d7d6a08fd26f8
initNirvana <kimqqyun@gmail.com>
2024-03-27 21:50:50 +0900

    feat: 쿠폰 생성 과정 수정

--- coupon/api/views.py
+++ coupon/api/views.py
@@ -1,16 +1,34 @@
 from rest_framework import permissions, generics
 from coupon.models import Coupon
 from coupon.api.serializers import CouponSerializer
+from rest_framework.exceptions import PermissionDenied
 
 # Create your views here.
 class CouponAPIView(generics.ListCreateAPIView):
     """
-    POST :
-        CODE : 실제 주문시 사용하는 쿠폰 코드
-        content : 쿠폰 설명
-        minimum_order_price : 최소 쿠폰 사용 금액
-        discount_price : 할인 금액
+    GET : 
+        쿠폰 리스트 조회
+    ```
+        - CODE : 실제 주문시 사용하는 쿠폰 코드
+        - content : 쿠폰 설명
+        - minimum_order_price : 최소 쿠폰 사용 금액
+        - discount_price : 할인 금액
+    ```
+    POST : 
+        쿠폰 추가 (주의)
+        쿠폰관리는 민감하므로 admin 유저만 허용 하였음
+    ```
+        - CODE : 실제 주문시 사용할 쿠폰 코드
+        - content : 쿠폰 설명
+        - minimum_order_price : 최소 쿠폰 사용 금액
+        - discount_price : 할인 금액
+    ```
     """
     permission_classes = [permissions.IsAuthenticated]
     queryset = Coupon.objects.all()
     serializer_class = CouponSerializer
+    
+    def create(self, request, *args, **kwargs):
+        if not request.user.is_admin:
+            raise PermissionDenied("You do not have permission to perform this action.")
+        return super().create(request, *args, **kwargs)
