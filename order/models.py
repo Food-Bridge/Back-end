cf803ad10e667abe4601d537b623f0cdd57ed655
장우진 <dnwls16071@naver.com>
2024-03-30 16:55:37 +0900

    Remove: 필드 옵션 일부 제거

--- order/models.py
+++ order/models.py
@@ -58,14 +58,10 @@ class Order(models.Model):
 
     # DecimalField
     latitude = models.DecimalField(
-        max_digits=9,
-        decimal_places=6,
         validators=[MinValueValidator(-90), MaxValueValidator(90)],
         null=True
     )
     longitude = models.DecimalField(
-        max_digits=9,
-        decimal_places=6,
         validators=[MinValueValidator(-180), MaxValueValidator(180)],
         null=True
     )
