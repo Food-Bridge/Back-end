1cc7528253f545a12b1ce11e895f499dbac09d4a
장우진 <dnwls16071@naver.com>
2024-03-30 16:43:47 +0900

    Refactor: 모델 필드 변경(FloatField → DecimalField)

--- order/models.py
+++ order/models.py
@@ -55,7 +55,8 @@ class Order(models.Model):
         verbose_name="주문서 상태",
         max_length=16
     )
-    
+
+    # DecimalField
     latitude = models.DecimalField(
         max_digits=9,
         decimal_places=6,
