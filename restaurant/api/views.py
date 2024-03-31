e22e60bcbfb714e9e07dd91437eb27a2925dc895
initNirvana <kimqqyun@gmail.com>
2024-03-31 20:46:58 +0900

    docs: 매장 POST 데이터 예시 수정

--- restaurant/api/views.py
+++ restaurant/api/views.py
@@ -14,7 +14,7 @@ class RestaurantAPIView(generics.ListCreateAPIView):
     ```
     {
         "name": "string2",
-        "address": "서울 관악구 난곡로 21길 10",
+        "address": "서울 영등포구 영중로 15 (타임스퀘어)",
         "phone_number": "021334294",
         "description": "string",
         "minimumOrderPrice": 3000,
@@ -29,6 +29,8 @@ class RestaurantAPIView(generics.ListCreateAPIView):
         "start": 24,
         "end": 36,
         "operatingTime": "string",
+        "minDeliveryTimeMinutes" : 10,
+        "maxDeliveryTimeMinutes" : 30,
         "deliveryFee": 3000,
         "mainCategory": 1,
         "subCategory": null
