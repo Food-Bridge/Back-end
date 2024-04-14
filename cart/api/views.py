46e7fe95b1c35f8b15c012c1cb0b137d2e654476
initNirvana <kimqqyun@gmail.com>
2024-04-14 14:59:43 +0900

    feat: Cart, 메뉴 선택 옵션 추가

--- cart/api/views.py
+++ cart/api/views.py
@@ -3,16 +3,16 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from cart.models import cart
 from restaurant.models import Restaurant
-from menu.models import Menu, MenuOption
+from menu.models import Menu, MenuOption, MenuSelectedOption
 from .serializers import CartSerializer
 
 class CartAPIView(generics.RetrieveUpdateDestroyAPIView):
     """
     1. menu_list : 메뉴 리스트
     2. option_list : 옵션 리스트
-    3. total_price : 최종 금액
-    4. deliveryman_request : 배송시 요청 사항
-    5. paymentMethod : credit_card, cash
+    3. soption_list : 선택 옵션 리스트
+    4. total_price : 최종 금액
+    5. deliveryman_request : 배송시 요청 사항
     6. restaurant : 레스토랑 id
     
     메뉴 2개, 옵션 2개 이상일 경우의 예
@@ -37,21 +37,20 @@ class CartAPIView(generics.RetrieveUpdateDestroyAPIView):
         "option_name": "소스 추가",
         "price": 1000,
         "quantity": 1
-        },
+        }
+    ],
+    "soption_list": [
         {
-        "option_id": 10,
-        "option_name": "치킨 무 추가",
-        "price": 0,
+        "option_id": 1,
+        "option_name": "소스 추가",
+        "price": 1000,
         "quantity": 1
         }
     ],
     "total_price": 0,
     "restaurant": 1
     }
-
-    ...
     ```
-    }
     """
     serializer_class = CartSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -71,6 +70,7 @@ class CartAPIView(generics.RetrieveUpdateDestroyAPIView):
         restaurant_id = request.data.get('restaurant')
         menu_data = request.data.get('menu_list', [])  # 메뉴 데이터 가져오기
         option_data = request.data.get('option_list', [])  # 옵션 데이터 가져오기
+        soption_data = request.data.get('soption_list', []) # 선택 옵션 데이터 가져오기
         
         # 장바구니에 저장할 음식점이 유효한지 확인
         try:
@@ -81,6 +81,7 @@ class CartAPIView(generics.RetrieveUpdateDestroyAPIView):
         # 메뉴와 옵션을 저장할 리스트 초기화
         menus = []
         options = []
+        soptions = []
         total_price = 0
         
         if len(menu_data) == 0:
@@ -110,12 +111,25 @@ class CartAPIView(generics.RetrieveUpdateDestroyAPIView):
             except MenuOption.DoesNotExist:
                 return Response({'error': '존재하지 않는 옵션이 주문되었습니다.'}, status=status.HTTP_400_BAD_REQUEST)
         
+        # 선택 옵션 데이터 처리
+        for option_item in soption_data:
+            option_id = option_item.get('option_id')
+            option_price = option_item.get('price')
+            option_quantity = option_item.get('quantity')
+            try:
+                option = MenuSelectedOption.objects.get(id=option_id)
+                soptions.append(option)
+                total_price += option_price * option_quantity  # 옵션 가격과 수량을 곱하여 총 가격에 더함
+            except MenuSelectedOption.DoesNotExist:
+                return Response({'error': '존재하지 않는 선택 옵션이 주문되었습니다'}, status=status.HTTP_400_BAD_REQUEST)
+
         # 장바구니 데이터 생성
         cart_data = {
             'user': request.user.id,
             'restaurant': request.data.get('restaurant'),
             'menu_list' : menu_data,
             'option_list' : option_data,
+            'soption_list' : soption_data,
             'total_price': total_price,
         }
         cart_serializer = CartSerializer(data=cart_data)
--- cart/models.py
+++ cart/models.py
@@ -7,5 +7,6 @@ class cart(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     menu_list = models.JSONField(blank=True, default=dict) # 메뉴
     option_list = models.JSONField(blank=True, null=True) # 옵션
+    soption_list = models.JSONField(blank=True, null=True) # 선택 옵션
     total_price = models.PositiveIntegerField(default=0)  # 주문의 총 가격
     created_at = models.DateTimeField(auto_now_add=True)
